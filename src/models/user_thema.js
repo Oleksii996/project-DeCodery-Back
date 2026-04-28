325d6f7ffc2a7ee3b0d2bf5d12df77fa73a615b8
Taras-Lisnyak <taras4lisnyak@gmail.com>
2026-04-28 19:44:45 +0300

    add gender as  theme

--- src/models/user_thema.js
+++ src/models/user_thema.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import { User } from "./user.js"; 
 
 const userThemaSchema = new mongoose.Schema({
   userId: { 
     type: mongoose.Schema.Types.ObjectId, 
     ref: "User", 
-    required: true 
+    required: true,
+    unique: true
   },
   themeColor: { 
     type: String, 
@@ -14,6 +16,21 @@ const userThemaSchema = new mongoose.Schema({
   updatedAt: { 
     type: Date, 
     default: Date.now 
+  }
+});
+
+userThemaSchema.pre("save", async function () {
+  if (this.isNew) {
+    const user = await User.findById(this.userId);
+    if (user) {
+      if (user.gender === "boy") {
+        this.themeColor = "blue";
+      } else if (user.gender === "girl") {
+        this.themeColor = "pink";
+      } else {
+        this.themeColor = "yellow"; // стандартний
+      }
+    }
   }
 });
 

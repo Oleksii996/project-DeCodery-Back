194e78b6e84c0eeb64d1db9aa162e8f5c9b868d9
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-04-29 10:27:23 +0200

    update code

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,32 +1,28 @@
-import { Router } from "express";
-import { celebrate } from "celebrate";
+import { Router } from 'express';
+import { celebrate } from 'celebrate';
 
-import { users as controller } from "../controllers/index.js";
-import { authenticate } from "../middleware/authenticate.js";
-import { updateCurrentUserSchema } from "../validations/index.js";
+import { users as controller } from '../controllers/index.js';
+import { updateCurrentUserSchema } from '../validations/index.js';
 
-const usersRouter = Router();
-
-usersRouter.get("/current", authenticate, controller.getCurrentUser);
-usersRouter.patch(
-  "/current",
-  authenticate,
-  celebrate(updateCurrentUserSchema),
-  controller.updateCurrentUser,
-);
-
-export default usersRouter;
-import { updateUserAvatar } from "../controllers/users/usersController.js";
+import { updateUserAvatar } from '../controllers/users/usersController.js';
 import { authenticate } from '../middleware/authenticate.js';
 import { upload } from '../middleware/upload.js';
 
 const usersRouter = Router();
 
+usersRouter.get('/current', authenticate, controller.getCurrentUser);
 usersRouter.patch(
-    '/me/avatar',
-    authenticate,
-    upload.single('avatar'),
-    updateUserAvatar,
-  );
+  '/current',
+  authenticate,
+  celebrate(updateCurrentUserSchema),
+  controller.updateCurrentUser,
+);
+
+usersRouter.patch(
+  '/me/avatar',
+  authenticate,
+  upload.single('avatar'),
+  updateUserAvatar,
+);
 
 export default usersRouter;

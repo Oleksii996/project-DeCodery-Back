7412e21b8a7c88e61d879dc61a6e3d22d13a62b9
Taras-Lisnyak <taras4lisnyak@gmail.com>
2026-05-01 19:51:30 +0300

    remove requierd

--- src/validations/usersThemaValidator.js
+++ src/validations/usersThemaValidator.js
@@ -6,7 +6,6 @@ export const validateTheme = celebrate({
   [Segments.BODY]: Joi.object().keys({
     color: Joi.string()
       .valid(...allowedColors)
-      .required()
       .messages({
         "any.only": `Некоректний колір. Доступні: ${allowedColors.join(", ")}`,
         "string.base": "Колір має бути рядком",

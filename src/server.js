2f18d01d00e86d5ed3c487aba3cd722025c63d0d
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-05-03 16:11:34 +0200

    update code

--- src/server.js
+++ src/server.js
@@ -22,15 +22,7 @@ const app = express();
 
 // Глобальні middleware
 app.use(express.json({ limit: '5mb' }));
-
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-  }),
-);
-
+app.use(cors({ methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'] }));
 app.use(helmet());
 app.use(cookieParser());
 

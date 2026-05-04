c77b451db3d35d8024d056a6ccb25cbf2986e1f3
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-05-04 17:17:41 +0200

    update code

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,20 +1,14 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 const mongoURL = process.env.MONGO_URL;
 
 export const connectMongoDB = async () => {
-  const mongoURL = process.env.MONGO_URL || "mongodb://localhost:27017/decodery";
+  const mongoURL =
+    process.env.MONGO_URL || 'mongodb://localhost:27017/decodery';
   try {
-    const mongoURI = process.env.MONGO_URI;
-
-    if (!mongoURI) {
-      throw new Error("MONGO_URI is not defined in .env");
-    }
-
-    await mongoose.connect(mongoURI);
-
-    console.log("MongoDB connected");
+    await mongoose.connect(mongoURL);
+    console.log('✅ MongoDB connection established successfully');
   } catch (error) {
-    console.error("Failed to connect to MongoDB:", error.message);
+    console.error(' Failed to connect to MongoDB:', error.message);
     process.exit(1);
   }
 };

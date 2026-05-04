53246d15241e804d89e5795788985b4104f5194f
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-05-04 17:27:48 +0200

    udapte code

--- src/routes/weeksRouter.js
+++ src/routes/weeksRouter.js
@@ -1,29 +1,29 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { authenticate } from "../middleware/authenticate.js";
+import { authenticate } from '../middleware/authenticate.js';
 
-import { getWeekData } from "../controllers/weeks/getWeekData.js";
-import { getPrivateWeekData } from "../controllers/weeks/getPrivateWeekData.js";
+import { getWeekData } from '../controllers/weeks/getWeekData.js';
+import { getPrivateWeekData } from '../controllers/weeks/getPrivateWeekData.js';
 
 import {
   getPublicWeeksController,
   getPrivateWeeksController,
   getBabyStateController,
   getMomStateController,
-} from "../controllers/weeks/weeksController.js";
+} from '../controllers/weeks/weeksController.js';
 
 const weeksRouter = Router();
 
-// 🔹 main (НЕ ЧІПАЄМО)
-weeksRouter.get("/", getPublicWeeksController);
-weeksRouter.get("/me", authenticate, getPrivateWeeksController);
+//main
+weeksRouter.get('/', getPublicWeeksController);
+weeksRouter.get('/me', authenticate, getPrivateWeeksController);
 
-// 🔹 існуючі роуті
-weeksRouter.get("/baby", authenticate, getBabyStateController);
-weeksRouter.get("/mom", authenticate, getMomStateController);
+//маршрути анни
+weeksRouter.get('/baby', authenticate, getBabyStateController);
+weeksRouter.get('/mom', authenticate, getMomStateController);
 
-// 🔹 нові (твій функціонал)
-weeksRouter.get("/private/:weekNumber", authenticate, getPrivateWeekData);
-weeksRouter.get("/:weekNumber", getWeekData);
+//маршрути віталії
+weeksRouter.get('/private/:weekNumber', authenticate, getPrivateWeekData);
+weeksRouter.get('/:weekNumber', getWeekData);
 
 export default weeksRouter;
